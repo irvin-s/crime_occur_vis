f01e3362724e314f0178ddd29d1ca89bd10c2e71
Irvin <isb@cin.ufpe.br>
2021-02-23 15:51:08 -0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -2,4 +2,9 @@ from flask import Flask
 
 app = Flask("CrimeViz")
 
+#App routes
+@app.route("/olamundo", methods=["post"])
+def olaMundo():
+    return{"msg": "Olá Mundo!"}
+
 app.run(host = '0.0.0.0')
