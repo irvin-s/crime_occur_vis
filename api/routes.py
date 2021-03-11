cb71a61b543ecb495084424274e419abe2098289
Irvin <61360847+irvin-s@users.noreply.github.com>
2021-03-11 11:37:34 -0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -4,15 +4,18 @@ import pandas as pd
 
 app = Flask("CrimeVis")
 
-def readXls():
-	df = pd.read_excel("../src/dataset.xls")
-	return(df)
 
-#App routes
+def readXls():
+    df = pd.read_excel("../src/dataset.xls")
+    return(df)
+
+# App routes
+
+
 @app.route("/getoccur", methods=["GET"])
 def getOccur():
     msg_j = json.dumps(readXls())
     return(msg_j)
 
-app.run()
 
+app.run()
