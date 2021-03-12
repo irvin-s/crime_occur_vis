42d12c968994adf298fa3569ecb0f20ead0725a0
Irvin <isb@cin.ufpe.br>
2021-03-12 17:53:56 -0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -5,9 +5,12 @@ import pandas as pd
 app = Flask("CrimeVis")
 
 
-def readXls(ini_month):
+def readXls(ini_month,tp_smart):
     df = pd.read_excel("../src/dataset.xls")
-    df = df.query('mes == "'+ini_month+'" & marca_celular == "APPLE"')
+    if tp_smart == "APPLE":
+        df = df.query('mes == "'+ini_month+'" & marca_celular == "APPLE"')
+    else:
+        df.query('mes == "'+ini_month+'"')
     df = df[['ano_bo', 'mes', 'latitude', 'longitude', 'rubrica', 'marca_celular']]
     json_msg = df.to_json(orient='index')
     return(json_msg)
@@ -18,6 +21,7 @@ def readXls(ini_month):
 @app.route("/getoccur", methods=["GET"])
 def getOccur():
     ini_month = request.args.get("ini_month")
+    ini_month = request.args.get("tp_smart")
     #msg_j = json.dumps(readXls())
     msg_j = readXls(ini_month)
     return(msg_j)
