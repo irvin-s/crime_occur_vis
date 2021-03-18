5d99d341e1048f184030ba3c7c4c010e4bb7ba7f
Irvin <61360847+irvin-s@users.noreply.github.com>
2021-03-17 23:51:48 -0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -16,7 +16,7 @@ def readXls(ini_month, brand):
         df = df.query('mes == "'+ini_month+'" & marca_celular != "APPLE"')
     else:
         df = df.query('mes == "'+ini_month+'"')
-    df = df[0:500]
+    df = df[0:1000]
     df = df[['ano_bo', 'mes', 'latitude', 'longitude', 'rubrica', 'marca_celular']]
     json_msg = df.to_json(orient='records')
     return(json_msg)
