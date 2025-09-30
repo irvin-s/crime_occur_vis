ca887e4e6c256782e2c8cf5a401df729b31cfeec
Irvin <61360847+irvin-s@users.noreply.github.com>
2025-09-30 18:15:04 -0300

    Remove commented-out legacy code in readXls function
    Cleaned up the readXls function by deleting old, commented-out code that used pandas for Excel processing. This improves code readability and maintainability.

--- api/data_class.py
+++ api/data_class.py
@@ -4,7 +4,6 @@ import duckdb
 
 #Create a function to convert xls duckbd database and filter by month and brand limited to 1000 records
 def readXls(ini_month, brand):
-    
     con = duckdb.connect(database=':memory:')
     if brand == "APPLE":
         query = f"""
@@ -31,17 +30,3 @@ def readXls(ini_month, brand):
     df = df.dropna(subset=['latitude', 'longitude'])
     json_msg = df.to_json(orient='records')
     return(json_msg)
-
-#def readXls(ini_month, brand):
-#    df = pd.read_excel("../data/dataset.xls")
-#    df = df.dropna(subset=['latitude', 'longitude'])
-#    if brand == "APPLE":
-#        df = df.query('mes == "'+ini_month+'" & marca_celular == "APPLE"')
-#    elif brand == "ANDROID":
-#        df = df.query('mes == "'+ini_month+'" & marca_celular != "APPLE"')
-#    else:
-#        df = df.query('mes == "'+ini_month+'"')
-#    df = df[0:1000]
-#    df = df[['ano_bo', 'mes', 'latitude', 'longitude', 'rubrica', 'marca_celular']]
-#    json_msg = df.to_json(orient='records')
-#    return(json_msg)
