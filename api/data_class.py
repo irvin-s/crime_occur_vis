a57cba957b7045d2cb4297ddb8ea31493c8c0bd3
Irvin <61360847+irvin-s@users.noreply.github.com>
2025-09-30 19:57:11 -0300

    Update data import to use xlsx and DuckDB excel extension
    Modified readXls to import data from 'dataset.xlsx' using DuckDB's excel extension instead of the previous xls method. Added installation and loading of the excel extension, and updated queries to use read_xlsx. Added 'dataset.xlsx' to the data directory.

--- api/data_class.py
+++ api/data_class.py
@@ -2,27 +2,29 @@ import json
 import pandas as pd
 import duckdb
 
-#Create a function to convert xls duckbd database and filter by month and brand limited to 1000 records
+#Import xlsx file into duckbd database and filter by month and brand limited to 1000 records
 def readXls(ini_month, brand):
     con = duckdb.connect(database=':memory:')
+    con.execute("INSTALL excel;")
+    con.execute("LOAD excel;")
     if brand == "APPLE":
         query = f"""
         SELECT ano_bo, mes, latitude, longitude, rubrica, marca_celular
-        FROM read_excel_auto('../data/dataset.xls')
+        FROM read_xlsx('../data/dataset.xlsx',  empty_as_varchar = true)
         WHERE mes = '{ini_month}' AND marca_celular = 'APPLE'
         LIMIT 1000
         """
     elif brand == "ANDROID":
         query = f"""
         SELECT ano_bo, mes, latitude, longitude, rubrica, marca_celular
-        FROM read_excel_auto('../data/dataset.xls')
+        FROM read_xlsx('../data/dataset.xlsx', empty_as_varchar = true)
         WHERE mes = '{ini_month}' AND marca_celular != 'APPLE'
         LIMIT 1000
         """
     else:
         query = f"""
         SELECT ano_bo, mes, latitude, longitude, rubrica, marca_celular
-        FROM read_excel_auto('../data/dataset.xls')
+        FROM read_xlsx('../data/dataset.xlsx', empty_as_varchar = true)
         WHERE mes = '{ini_month}'
         LIMIT 1000
         """
