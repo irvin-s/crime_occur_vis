3aae1f8e482d09ca4d96969165352a7699be4d0d
Irvin <61360847+irvin-s@users.noreply.github.com>
2025-09-30 17:49:32 -0300

    Refactor XLS reading to use DuckDB for filtering
    Replaced the pandas-based XLS reading and filtering logic with a new function that uses DuckDB to query and filter the dataset directly from the Excel file, improving performance and scalability. Limited results to 1000 records and added DuckDB to requirements.txt.

--- api/data_class.py
+++ api/data_class.py
@@ -1,16 +1,47 @@
 import json
 import pandas as pd
+import duckdb
 
-def readXls(ini_month, brand):
-    df = pd.read_excel("../data/dataset.xls")
-    df = df.dropna(subset=['latitude', 'longitude'])
+#Create a function to convert xls duckbd database and filter by month and brand limited to 1000 records
+def readXlsFromDuckDB(ini_month, brand):
+    
+    con = duckdb.connect(database=':memory:')
     if brand == "APPLE":
-        df = df.query('mes == "'+ini_month+'" & marca_celular == "APPLE"')
+        query = f"""
+        SELECT ano_bo, mes, latitude, longitude, rubrica, marca_celular
+        FROM read_excel_auto('../data/dataset.xls')
+        WHERE mes = '{ini_month}' AND marca_celular = 'APPLE'
+        LIMIT 1000
+        """
     elif brand == "ANDROID":
-        df = df.query('mes == "'+ini_month+'" & marca_celular != "APPLE"')
+        query = f"""
+        SELECT ano_bo, mes, latitude, longitude, rubrica, marca_celular
+        FROM read_excel_auto('../data/dataset.xls')
+        WHERE mes = '{ini_month}' AND marca_celular != 'APPLE'
+        LIMIT 1000
+        """
     else:
-        df = df.query('mes == "'+ini_month+'"')
-    df = df[0:1000]
-    df = df[['ano_bo', 'mes', 'latitude', 'longitude', 'rubrica', 'marca_celular']]
+        query = f"""
+        SELECT ano_bo, mes, latitude, longitude, rubrica, marca_celular
+        FROM read_excel_auto('../data/dataset.xls')
+        WHERE mes = '{ini_month}'
+        LIMIT 1000
+        """
+    df = con.execute(query).fetchdf()
+    df = df.dropna(subset=['latitude', 'longitude'])
     json_msg = df.to_json(orient='records')
     return(json_msg)
+
+#def readXls(ini_month, brand):
+#    df = pd.read_excel("../data/dataset.xls")
+#    df = df.dropna(subset=['latitude', 'longitude'])
+#    if brand == "APPLE":
+#        df = df.query('mes == "'+ini_month+'" & marca_celular == "APPLE"')
+#    elif brand == "ANDROID":
+#        df = df.query('mes == "'+ini_month+'" & marca_celular != "APPLE"')
+#    else:
+#        df = df.query('mes == "'+ini_month+'"')
+#    df = df[0:1000]
+#    df = df[['ano_bo', 'mes', 'latitude', 'longitude', 'rubrica', 'marca_celular']]
+#    json_msg = df.to_json(orient='records')
+#    return(json_msg)
